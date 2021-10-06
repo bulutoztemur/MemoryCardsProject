ca780ea83ee7577ac34abfe9acc4c0d10fa08707
ALAATTIO <alaattinbulut.oztemur@intertech.com.tr>
2021-10-07 01:08:45 +0300

    redundant code removed

--- MemoryCardsProject/Application/GameBoard/GameBoardVC.swift
+++ MemoryCardsProject/Application/GameBoard/GameBoardVC.swift
@@ -36,15 +36,6 @@ class GameBoardVC: BaseVC {
         return view
     }()
     
-    init() {
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGreen
